0fad4503c2729f4ec5e17e8f063a9a445ebaa674
intersoom <sallybig@naver.com>
2023-09-11 04:37:25 +0900

    ✨ TopBar 컴포넌트 제작

---
+++ src/components/common/TopBar.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+
+import Spacing from './Spacing'
+
+import leftIcon from '../../assets/leftIcon.svg'
+import ticket from '../../assets/ticket.svg'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  backNav: string
+  title: string
+  ticketCount: number
+}
+
+const TopBar = ({ backNav, title, ticketCount, ...props }: Props) => {
+  const navigate = useNavigate()
+  return (
+    <div className="w-screen h-[44px] flex flex-row items-center justify-between" {...props}>
+      <div className="w-[50px] h-[50px] flex justify-center items-center">
+        <img
+          src={leftIcon as string}
+          className="w-[24px] h-[24px]"
+          alt="뒤로가기"
+          onClick={() => navigate(backNav)}
+        ></img>
+      </div>
+      <span className="text-black font-body1">{title}</span>
+      <div className="flex flex-row items-center">
+        <img
+          src={ticket as string}
+          className="h-[22px]"
+          alt="티켓 아이콘"
+          onClick={() => navigate(backNav)}
+        ></img>
+        <Spacing direction="horizontal" size={4} />
+        <span className="text-black font-body2">x </span>
+        <span className="text-pink font-body2">{ticketCount}</span>
+        <Spacing direction="horizontal" size={12.87} />
+      </div>
+    </div>
+  )
+}
+
+export default TopBar

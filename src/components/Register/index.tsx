aaaaea3c6b9f206eda29b3918729fee44ccc328a
nijuy <youjin6325@naver.com>
2023-09-10 23:05:15 +0900

    ✨ useMultistepForm을 사용한 단계 이동 기능 구현

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,5 +1,46 @@
+import { FormEvent } from 'react'
+
+import useMultistepForm from '../../hooks/useMultistepForm'
+
 const Register = () => {
-  return <div>등록하기</div>
+  const { currentStep, next } = useMultistepForm([
+    <div key="gender">
+      성별
+      <br />
+      <button type="button" onClick={moveNextStep}>
+        다음 단계로
+      </button>
+    </div>,
+    <div key="animal">
+      동물
+      <br />
+      <button type="button" onClick={moveNextStep}>
+        다음 단계로
+      </button>
+    </div>,
+    <div key="introduce">
+      자기소개
+      <br />
+      <button type="submit" onClick={moveNextStep}>
+        등록하기
+      </button>
+    </div>,
+  ])
+
+  function moveNextStep() {
+    next()
+  }
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault()
+    alert('등록 완료! 둘러보기에서 다른 프로필을 구경해보세요 👀')
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>{currentStep}</form>
+    </div>
+  )
 }
 
 export default Register

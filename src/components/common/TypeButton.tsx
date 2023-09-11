d2f9b5b5e393be538252deb6b0fd323f84ad07d8
intersoom <sallybig@naver.com>
2023-09-11 22:11:07 +0900

    💄 flex-col 추가

--- src/components/common/TypeButton.tsx
+++ src/components/common/TypeButton.tsx
@@ -1,14 +1,21 @@
 import { ReactElement } from 'react'
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {
+interface TypeButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   mode?: 'white' | 'pink'
   width?: number
   height?: number
   children: ReactElement
 }
 
-const TypeButton = ({ mode = 'white', width = 161, height = 161, children, ...props }: Props) => {
-  const defaultStyle = 'shadow-typeBtn rounded-[20px] flex justify-center items-center font-body2'
+const TypeButton = ({
+  mode = 'white',
+  width = 161,
+  height = 161,
+  children,
+  ...props
+}: TypeButtonProps) => {
+  const defaultStyle =
+    'shadow-typeBtn rounded-[20px] flex justify-center items-center font-body2 flex-col'
   const modeStyle = {
     pink: 'bg-pink text-white',
     white: 'bg-white text-pink',

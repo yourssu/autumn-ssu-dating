c08a3571e7c74e60f1ebdb2e4f72dfeae432f185
intersoom <sallybig@naver.com>
2023-09-12 02:09:38 +0900

    💄 티켓 갯수와 X 사이의 거리 간격 조정

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -35,7 +35,8 @@ const TopBar = ({ backNav, title, ticketCount, ...props }: TopBarProps) => {
           onClick={() => navigate(backNav)}
         ></img>
         <Spacing direction="horizontal" size={4} />
-        <span className="text-black font-body2">x </span>
+        <span className="text-black font-body2">x</span>
+        <Spacing direction="horizontal" size={5} />
         <span className="text-pink font-body2">{ticketCount}</span>
         <Spacing direction="horizontal" size={12.87} />
       </div>

c1a5601aa1280214554921ee17474ba1b64a089e
nijuy <youjin6325@naver.com>
2023-09-12 01:58:24 +0900

    ✨ 이름, 어필 현재 글자 수 추가

--- src/components/Register/PersonalInfoStep.tsx
+++ src/components/Register/PersonalInfoStep.tsx
@@ -6,9 +6,12 @@ const PersonalInfoStep = ({ nickname, mbti, appeal, tel, updateFields }: FormSte
       <p>아래 정보들을 입력해 본인을 소개해보세요!</p>
 
       <div className="grid grid-cols-1 gap-y-2">
-        <label>이름</label>
+        <label>
+          이름 ( <span>{nickname.length}</span> / 10 )
+        </label>
         <input
           required
+          maxLength={10}
           placeholder="ex. 숭실대 뿌슝이"
           value={nickname}
           onChange={(e) => updateFields({ nickname: e.target.value })}
@@ -18,9 +21,12 @@ const PersonalInfoStep = ({ nickname, mbti, appeal, tel, updateFields }: FormSte
         <label>MBTI</label>
         {mbti}
 
-        <label>본인 매력 어필</label>
+        <label>
+          본인 매력 어필 ( <span>{appeal.length}</span> / 100 )
+        </label>
         <textarea
           required
+          maxLength={100}
           value={appeal}
           onChange={(e) => updateFields({ appeal: e.target.value })}
           className="border"

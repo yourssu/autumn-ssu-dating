b00624f9b6a572825f1a97827fa472dc3a1e49ec
intersoom <sallybig@naver.com>
2023-09-12 02:44:12 +0900

    🏷️ CurrentExploreFilterType, AnimalType 추가

--- src/types/explore.type.ts
+++ src/types/explore.type.ts
@@ -1,3 +1,12 @@
-export type SelectedType = 'male' | 'female'
 export type IsSelectedType = 'selected' | 'notSelected'
 export type GenderType = 'male' | 'female'
+
+export type MaleAnimalType = 'fox' | 'cat'
+export type FemaleAnimalType = 'rabbit' | 'dog'
+
+export type AnimalType = MaleAnimalType | FemaleAnimalType
+
+export interface CurrentExploreFilterType {
+  gender: GenderType
+  animal: AnimalType
+}

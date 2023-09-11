a8d5809da8e4be34a290c82447a435f8dc334fb9
intersoom <sallybig@naver.com>
2023-09-12 00:47:20 +0900

    ✨ 레이아웃 제작

---
+++ src/constant/index.ts
@@ -0,0 +1,6 @@
+export const LINK_TITLE: { [key: string]: { title: string; backNav?: string } } = {
+  '/': { title: '홈' },
+  '/explore': { title: '둘러보기', backNav: '/' },
+  // 임시
+  '/register': { title: '등록하기', backNav: '/' },
+}

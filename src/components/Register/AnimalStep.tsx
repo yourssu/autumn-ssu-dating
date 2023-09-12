1423670c6fe86ef93bd5aa245339a0b20b82aabf
nijuy <youjin6325@naver.com>
2023-09-12 15:32:54 +0900

    💄 AnimalStep에 TypeButton 적용

--- src/components/Register/AnimalStep.tsx
+++ src/components/Register/AnimalStep.tsx
@@ -1,12 +1,27 @@
 import ImageButton from './ImageButton'
 
 import { FormStepProps } from '../../types/register.type'
+import TypeButton from '../common/TypeButton'
 
 const AnimalStep = ({ gender, updateFields, moveNextStep }: FormStepProps) => {
   const animalOptions =
     gender === '남자'
-      ? ['강아지', '여우', '늑대', '공룡', '곰', '뿌슝이']
-      : ['강아지', '고양이', '여우', '토끼', '햄스터', '뿌슝이']
+      ? [
+          { src: '/src/assets/dogIcon.png', label: '강아지' },
+          { src: '/src/assets/foxIcon.png', label: '여우' },
+          { src: '/src/assets/wolfIcon.png', label: '늑대' },
+          { src: '/src/assets/dinoIcon.png', label: '공룡' },
+          { src: '/src/assets/bearIcon.png', label: '곰' },
+          { src: '/src/assets/ppussungIcon.png', label: '뿌슝이' },
+        ]
+      : [
+          { src: '/src/assets/dogIcon.png', label: '강아지' },
+          { src: '/src/assets/catIcon.png', label: '고양이' },
+          { src: '/src/assets/foxIcon.png', label: '여우' },
+          { src: '/src/assets/rabbitIcon.png', label: '토끼' },
+          { src: '/src/assets/hamsterIcon.png', label: '햄스터' },
+          { src: '/src/assets/ppussungIcon.png', label: '뿌슝이' },
+        ]
 
   const onClick = (e: React.MouseEvent) => {
     updateFields({
@@ -22,7 +37,9 @@ const AnimalStep = ({ gender, updateFields, moveNextStep }: FormStepProps) => {
       <div>자신을 나타내는 동물을 골라주세요.</div>
       <div>
         {animalOptions.map((option) => (
-          <ImageButton key={option} label={option} onClick={onClick} />
+          <TypeButton key={option.label}>
+            <ImageButton src={option.src} label={option.label} onClick={onClick} />
+          </TypeButton>
         ))}
       </div>
     </>

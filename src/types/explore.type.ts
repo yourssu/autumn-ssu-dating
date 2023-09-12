e60cab42036e71e221c49f259c95e9c817eb0caa
intersoom <sallybig@naver.com>
2023-09-13 01:13:21 +0900

    🏷️ type 전체 추가 및 동물 남녀 구분

--- src/types/explore.type.ts
+++ src/types/explore.type.ts
@@ -1,12 +1,15 @@
 export type IsSelectedType = 'selected' | 'notSelected'
 export type GenderType = 'male' | 'female'
 
-export type MaleAnimalType = 'dog' | 'fox' | 'wolf' | 'dragon' | 'bear' | 'ppussung'
-export type FemaleAnimalType = 'dog' | 'cat' | 'fox' | 'rabbit' | 'hamster' | 'ppussung'
+export type MaleAnimalType = '전체' | '강아지' | '여우' | '늑대' | '공룡' | '곰' | '뿌슝이'
+export type FemaleAnimalType = '전체' | '강아지' | '고양이' | '여우' | '토끼' | '햄스터' | '뿌슝이'
 
 export type AnimalType = MaleAnimalType | FemaleAnimalType
 
 export interface CurrentExploreFilterType {
   gender: GenderType
-  animal: AnimalType
+  maleAnimal: MaleAnimalType
+  femaleAnimal: FemaleAnimalType
 }
+
+export type AnimalTabMode = 'image' | 'text'

fe737395d7854cc1b4e74014f06659690280a173
nijuy <youjin6325@naver.com>
2023-09-12 15:20:14 +0900

    💄 GenderStep에 TypeButton 적용

--- src/components/Register/GenderStep.tsx
+++ src/components/Register/GenderStep.tsx
@@ -1,9 +1,13 @@
 import ImageButton from './ImageButton'
 
 import { FormStepProps } from '../../types/register.type'
+import TypeButton from '../common/TypeButton'
 
 const GenderStep = ({ updateFields, moveNextStep }: FormStepProps) => {
-  const genderOptions = ['남자', '여자']
+  const genderOptions = [
+    { src: '/src/assets/maleIcon.png', label: '남자' },
+    { src: '/src/assets/femaleIcon.png', label: '여자' },
+  ]
 
   const onClick = (e: React.MouseEvent) => {
     updateFields({
@@ -19,7 +23,9 @@ const GenderStep = ({ updateFields, moveNextStep }: FormStepProps) => {
       <div>당신의 성별은?</div>
       <div>
         {genderOptions.map((option) => (
-          <ImageButton key={option} label={option} onClick={onClick} />
+          <TypeButton key={option.label}>
+            <ImageButton src={option.src} label={option.label} onClick={onClick} />
+          </TypeButton>
         ))}
       </div>
     </>

0c93d1ab48d442626d1587749f849a9118656418
nijuy <youjin6325@naver.com>
2023-09-15 00:04:46 +0900

    ✨ TextareaField 컴포넌트 제작

---
+++ src/components/Register/TextareaField.tsx
@@ -0,0 +1,19 @@
+interface TextareaFieldProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  height?: number
+}
+
+const TextareaField = ({ height = 127, ...props }: TextareaFieldProps) => {
+  const defaultStyle =
+    'p-4 bg-white rounded-xl text-pink placeholder-gray font-body2 resize-none focus:outline-none focus:shadow-inputField'
+  const shadowStyle = !props.value && 'shadow-inputField'
+
+  return (
+    <textarea
+      {...props}
+      className={`${defaultStyle} ${shadowStyle}`}
+      style={{ height: `${height}px` }}
+    />
+  )
+}
+
+export default TextareaField

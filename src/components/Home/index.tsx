b79555f71b44effe33450f0ab808e1cc0c48a629
nijuy <youjin6325@naver.com>
2023-09-16 02:59:01 +0900

    💄 Spacing 컴포넌트로 인증코드 사이 간격 주기, 불필요한 div 삭제

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -26,7 +26,7 @@ const Home = () => {
         {'뿌슝이의\n동물 SSU개팅'}
       </p>
       <Spacing direction="vertical" size={15} />
-      <div className="grid grid-flow-col gap-x-2 w-fit">
+      <div className="flex">
         <InputField
           width={218}
           value={code}
@@ -35,6 +35,7 @@ const Home = () => {
           }}
           placeholder="인증코드를 입력해주세요."
         />
+        <Spacing direction="horizontal" size={8} />
         <BoxButton isLine="line" size="extraSmall">
           <button
             className="w-full h-full rounded-2xl text-body2 focus:outline-none"
@@ -45,45 +46,43 @@ const Home = () => {
         </BoxButton>
       </div>
       <Spacing direction="vertical" size={12} />
-      <div>
-        <div className="w-[342px] flex justify-center text-body2">
-          <img src={ticket as string} className="h-[22px]" alt="티켓 아이콘" />
-          <Spacing direction="horizontal" size={4} />
-          <p>
-            이용권 x <span className="text-pink">1</span>
-          </p>
-        </div>
-        <Spacing direction="vertical" size={40} />
-        <div className="grid grid-flow-col gap-x-5 overflow-scroll w-[343px]">
-          {animalOptions.map((option, index) => (
-            <TypeButton key={index}>
-              <img src={option.src} />
-            </TypeButton>
-          ))}
-        </div>
-        <Spacing direction="vertical" size={48} />
-        <BoxButton size="large">
-          <button
-            className="w-full h-full"
-            onClick={() => {
-              navigate('/register')
-            }}
-          >
-            프로필 등록하기
-          </button>
-        </BoxButton>
-        <Spacing direction="vertical" size={16} />
-        <BoxButton size="large">
-          <button
-            className="w-full h-full"
-            onClick={() => {
-              navigate('/explore')
-            }}
-          >
-            내 이상형 찾기
-          </button>
-        </BoxButton>
+      <div className="w-[342px] flex justify-center text-body2">
+        <img src={ticket as string} className="h-[22px]" alt="티켓 아이콘" />
+        <Spacing direction="horizontal" size={4} />
+        <p>
+          이용권 x <span className="text-pink">1</span>
+        </p>
       </div>
+      <Spacing direction="vertical" size={40} />
+      <div className="grid grid-flow-col gap-x-5 overflow-scroll w-[343px]">
+        {animalOptions.map((option, index) => (
+          <TypeButton key={index}>
+            <img src={option.src} />
+          </TypeButton>
+        ))}
+      </div>
+      <Spacing direction="vertical" size={48} />
+      <BoxButton size="large">
+        <button
+          className="w-full h-full"
+          onClick={() => {
+            navigate('/register')
+          }}
+        >
+          프로필 등록하기
+        </button>
+      </BoxButton>
+      <Spacing direction="vertical" size={16} />
+      <BoxButton size="large">
+        <button
+          className="w-full h-full"
+          onClick={() => {
+            navigate('/explore')
+          }}
+        >
+          내 이상형 찾기
+        </button>
+      </BoxButton>
     </div>
   )
 }

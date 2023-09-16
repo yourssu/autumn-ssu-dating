62a36c2ee1cbc3a093c17576ff6926333d8a3c1e
intersoom <sallybig@naver.com>
2023-09-16 18:11:03 +0900

    ✨ usePopup 훅 제작

---
+++ src/hooks/usePopup.ts
@@ -0,0 +1,47 @@
+import { MutableRefObject, useState } from 'react'
+
+import { CurrentPopupSelectedType } from '../types/explore.type'
+
+const usePopup = () => {
+  const [isPopup, setIsPopup] = useState<boolean>(false)
+  const [currentPopupSelected, setCurrentPopupSelected] = useState<CurrentPopupSelectedType>({
+    nickname: 'ppussung',
+    mbti: 'ISTJ',
+    gender: 'female',
+    contact: '@yourssu_official',
+    animal: '뿌슝이',
+    content: 'ppussung',
+  })
+
+  const handlePopup = (
+    bgRef: MutableRefObject<HTMLDivElement | null>,
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (bgRef.current && e) {
+      if (bgRef.current == (e.target as Node)) {
+        setIsPopup(false)
+      }
+    }
+  }
+
+  const handlePopupSelected = (e: CurrentPopupSelectedType) => {
+    setCurrentPopupSelected(e)
+    setIsPopup(true)
+  }
+
+  const handleClosePopup = () => {
+    setIsPopup(false)
+  }
+
+  return {
+    isPopup,
+    setIsPopup,
+    handlePopup,
+    handleClosePopup,
+    handlePopupSelected,
+    currentPopupSelected,
+    setCurrentPopupSelected,
+  }
+}
+
+export default usePopup

752f3239b63e41f9648839d6bf11c99c18e16fef
intersoom <sallybig@naver.com>
2023-09-16 18:07:00 +0900

    🔥 불필요한 코드 삭제

--- src/components/common/TypeButton.tsx
+++ src/components/common/TypeButton.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, forwardRef } from 'react'
+import { ReactElement } from 'react'
 
 interface TypeButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   mode?: 'white' | 'pink'
@@ -8,37 +8,29 @@ interface TypeButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   children: ReactElement
 }
 
-const TypeButton = forwardRef<HTMLDivElement, TypeButtonProps>(
-  (
-    {
-      mode = 'white',
-      width = 161,
-      height = 161,
-      rounded = 20,
-      children,
-      ...props
-    }: TypeButtonProps,
-    ref
-  ) => {
-    const defaultStyle = 'shadow-typeBtn flex justify-center items-center font-body2 flex-col'
-    const modeStyle = {
-      pink: 'bg-pink text-white',
-      white: 'bg-white text-pink',
-    }
-
-    return (
-      <div
-        {...props}
-        style={{ width: `${width}px`, height: `${height}px`, borderRadius: `${rounded}px` }}
-        className={`${defaultStyle} ${modeStyle[mode]} ${props.className}`}
-        ref={ref}
-      >
-        {children}
-      </div>
-    )
+const TypeButton = ({
+  mode = 'white',
+  width = 161,
+  height = 161,
+  rounded = 20,
+  children,
+  ...props
+}: TypeButtonProps) => {
+  const defaultStyle = 'shadow-typeBtn flex justify-center items-center font-body2 flex-col'
+  const modeStyle = {
+    pink: 'bg-pink text-white',
+    white: 'bg-white text-pink',
   }
-)
 
-TypeButton.displayName = 'TypeButton'
+  return (
+    <div
+      {...props}
+      style={{ width: `${width}px`, height: `${height}px`, borderRadius: `${rounded}px` }}
+      className={`${defaultStyle} ${modeStyle[mode]} ${props.className}`}
+    >
+      {children}
+    </div>
+  )
+}
 
 export default TypeButton

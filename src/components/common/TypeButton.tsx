e2a74a176df0edbc53cdd35874dfc69b5504cbef
intersoom <sallybig@naver.com>
2023-09-16 17:48:06 +0900

    ⚡️ props를 통한 className이 수정 가능하게함

--- src/components/common/TypeButton.tsx
+++ src/components/common/TypeButton.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { ReactElement, forwardRef } from 'react'
 
 interface TypeButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   mode?: 'white' | 'pink'
@@ -8,29 +8,37 @@ interface TypeButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   children: ReactElement
 }
 
-const TypeButton = ({
-  mode = 'white',
-  width = 161,
-  height = 161,
-  rounded = 20,
-  children,
-  ...props
-}: TypeButtonProps) => {
-  const defaultStyle = 'shadow-typeBtn flex justify-center items-center font-body2 flex-col'
-  const modeStyle = {
-    pink: 'bg-pink text-white',
-    white: 'bg-white text-pink',
-  }
+const TypeButton = forwardRef<HTMLDivElement, TypeButtonProps>(
+  (
+    {
+      mode = 'white',
+      width = 161,
+      height = 161,
+      rounded = 20,
+      children,
+      ...props
+    }: TypeButtonProps,
+    ref
+  ) => {
+    const defaultStyle = 'shadow-typeBtn flex justify-center items-center font-body2 flex-col'
+    const modeStyle = {
+      pink: 'bg-pink text-white',
+      white: 'bg-white text-pink',
+    }
 
-  return (
-    <div
-      style={{ width: `${width}px`, height: `${height}px`, borderRadius: `${rounded}px` }}
-      className={`${defaultStyle} ${modeStyle[mode]}`}
-      {...props}
-    >
-      {children}
-    </div>
-  )
-}
+    return (
+      <div
+        {...props}
+        style={{ width: `${width}px`, height: `${height}px`, borderRadius: `${rounded}px` }}
+        className={`${defaultStyle} ${modeStyle[mode]} ${props.className}`}
+        ref={ref}
+      >
+        {children}
+      </div>
+    )
+  }
+)
+
+TypeButton.displayName = 'TypeButton'
 
 export default TypeButton

e0c17d0380a52f77b06a5fed7e794b292dcc6155
intersoom <sallybig@naver.com>
2023-09-18 01:58:09 +0900

    ⚡️ className 커스터마이징 가능하게 수정

--- src/components/common/ToastMessage.tsx
+++ src/components/common/ToastMessage.tsx
@@ -7,8 +7,8 @@ interface ToastMessageProps extends React.HTMLAttributes<HTMLDivElement> {
 const ToastMessage = ({ children, ...props }: ToastMessageProps) => {
   return (
     <div
-      className="w-[374px] h-[50px] mb-[22px] py-4 bg-black text-body2 text-white rounded-[8px] flex justify-center items-center animate-fade-up"
       {...props}
+      className={`w-[374px] h-[50px] mb-[22px] py-4 bg-black text-body2 text-white rounded-[8px] flex justify-center items-center animate-fade-up ${props.className}`}
     >
       {children}
     </div>

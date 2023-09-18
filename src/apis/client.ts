8c6e88223cfd08c3e87f26b4ef62927d8570c02c
nijuy <youjin6325@naver.com>
2023-09-18 20:22:39 +0900

    👽️ api url 수정 (http->https)

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://ssudate-server.yourssu.com/',
+  baseURL: 'https://ssudate-server.yourssu.com/',
 })
 
 export default client

e997e15dbb1d66b23b78a2ec4236ccc92280af5b
nijuy <youjin6325@naver.com>
2023-09-18 20:17:47 +0900

    👽️ client baseURL 수정

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'https://ssudate.xodns.site:443',
+  baseURL: 'http://ssudate-server.yourssu.com/',
 })
 
 export default client

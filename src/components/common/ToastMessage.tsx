f901bd700df1af4b1d60ab1366dd6eaca6d66b2c
nijuy <youjin6325@naver.com>
2023-09-19 12:23:22 +0900

    💄 중앙 정렬 방식 변경 (flex -> text-center)

--- src/components/common/ToastMessage.tsx
+++ src/components/common/ToastMessage.tsx
@@ -8,7 +8,7 @@ const ToastMessage = ({ children, ...props }: ToastMessageProps) => {
   return (
     <div
       {...props}
-      className={`w-[calc(100%-16px)] max-w-[374px] h-[50px] mb-[22px] py-4 bg-black text-body2 text-white rounded-[8px] flex justify-center items-center animate-fade-up ${props.className}`}
+      className={`w-[calc(100%-16px)] max-w-[374px] h-[50px] mb-[22px] py-4 bg-black text-body2 text-white text-center rounded-[8px] animate-fade-up ${props.className}`}
     >
       {children}
     </div>

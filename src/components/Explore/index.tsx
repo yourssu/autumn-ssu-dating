f1412cd4e0d923c06441b40d1a244dca972760ee
intersoom <sallybig@naver.com>
2023-09-19 17:19:24 +0900

    🔇 불필요한 로그 삭제

--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -32,10 +32,6 @@ const Explore = () => {
       : currentExploreFilter.maleAnimal
   )
 
-  // useEffect(() => {
-  //   console.log(error)
-  // }, [error])
-
   useEffect(() => {
     hideRecoilStateToast()
   }, [recoilStateToast, hideRecoilStateToast])

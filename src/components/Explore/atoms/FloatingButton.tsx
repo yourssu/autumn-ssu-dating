0ff2ad3951acd246ddfcb07ca5799f47a5a61d66
intersoom <sallybig@naver.com>
2023-09-19 17:26:46 +0900

    ⚡️ onClick 사용을 위해서 buttonAttribute Props 상속

--- src/components/Explore/atoms/FloatingButton.tsx
+++ src/components/Explore/atoms/FloatingButton.tsx
@@ -1,8 +1,11 @@
 import Reload from '../../../assets/reload.svg'
 
-const FloatingButton = () => {
+interface FloatingButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
+
+const FloatingButton = ({ ...props }: FloatingButtonProps) => {
   return (
     <button
+      {...props}
       style={{ WebkitTapHighlightColor: 'transparent' }}
       className="w-16 h-16 p-[18px] bg-white rounded-[999px] shadow-[0px_2px_8px_0px_rgba(0,0,0,0.15)] border border-lightPink absolute bottom-[16px] right-[16px]"
     >

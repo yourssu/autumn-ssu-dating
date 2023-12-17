94481229564f2597f80bbffde01a0acecaa80981
nijuy <youjin6325@naver.com>
2023-12-18 00:40:21 +0900

    🏷️ UserInfoResponse 타입 생성
    - 추천인 코드 등록, 마이페이지에서 사용

---
+++ src/types/user.type.ts
@@ -0,0 +1,13 @@
+export interface UserInfoResponse {
+  animals: string
+  code: string
+  contact: string
+  createdAt: string
+  gender: string
+  id: number
+  introduce: string
+  mbti: string
+  nickName: string
+  ticket: number
+  weight: number
+}

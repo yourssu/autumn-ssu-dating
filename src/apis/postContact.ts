1b6f44341c0ed511defe7449025b588ff175bcb3
nijuy <youjin6325@naver.com>
2023-12-18 19:58:54 +0900

    ♻️ PopupModal에서 ticketList 떼기
    - api에서도 인증코드 제거

--- src/apis/postContact.ts
+++ src/apis/postContact.ts
@@ -2,15 +2,8 @@ import client from './client'
 
 import { ContactResponse } from '../types/postContact.type'
 
-export const postContact = async ({
-  code,
-  nickName,
-}: {
-  code: string
-  nickName: string
-}): Promise<ContactResponse> => {
+export const postContact = async ({ nickName }: { nickName: string }): Promise<ContactResponse> => {
   const response = await client.post('/contact', {
-    code,
     nickName,
   })
   return response.data

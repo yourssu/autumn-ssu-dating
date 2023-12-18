7418da6ea599099b722f1543db43d47f59d9febf
intersoom <sallybig@naver.com>
2023-12-19 03:12:43 +0900

    ✨ 토큰 관리 기능 추가

--- src/components/Redirect/index.tsx
+++ src/components/Redirect/index.tsx
@@ -2,8 +2,12 @@ import { useEffect } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
+import { setToken } from '../../utils/tokenUtils'
+
 const Redirect = () => {
   const code = new URL(document.location.toString()).searchParams.get('oauthName')
+  const accessToken = new URL(document.location.toString()).searchParams.get('accessToken')
+  const refreshToken = new URL(document.location.toString()).searchParams.get('refreshToken')
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -11,6 +15,10 @@ const Redirect = () => {
       navigate('/register', { state: { code } })
     }
   }, [code])
+
+  useEffect(() => {
+    if (accessToken && refreshToken) setToken(accessToken, refreshToken)
+  }, [accessToken, refreshToken])
 
   return <></>
 }

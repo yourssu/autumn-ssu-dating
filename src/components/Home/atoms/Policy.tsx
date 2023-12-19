db05f8d0c1216bcf4593fb9b716d9d22ddeabd25
nijuy <youjin6325@naver.com>
2023-12-19 21:46:47 +0900

    ✨ Policy에 노션 링크 추가

--- src/components/Home/atoms/Policy.tsx
+++ src/components/Home/atoms/Policy.tsx
@@ -1,3 +1,5 @@
+import { POLICY_LINK, PRIVACY_LINK } from '../../../constant'
+
 const Link = ({ link, children }: { link: string; children: string }) => {
   return (
     <a
@@ -14,8 +16,8 @@ const Link = ({ link, children }: { link: string; children: string }) => {
 const Policy = () => {
   return (
     <p className="whitespace-pre-line text-center text-caption">
-      로그인할 경우 SSU개팅 <Link link="link1">서비스 이용약관</Link>과{'\n'}
-      <Link link="link2"> 개인정보 처리방침</Link>에 동의하는 것으로 간주됩니다.
+      로그인할 경우 SSU개팅 <Link link={POLICY_LINK}>서비스 이용약관</Link>과{'\n'}
+      <Link link={PRIVACY_LINK}> 개인정보 처리방침</Link>에 동의하는 것으로 간주됩니다.
     </p>
   )
 }

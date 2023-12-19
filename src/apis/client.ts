ae3fb2de8b01fd26866b36693c14d85500863686
nijuy <youjin6325@naver.com>
2023-12-19 15:47:48 +0900

    ✨ 토큰 재발급을 위한 response interceptor 생성
    - 토큰 재발급 api 추가, 만료 여부 확인을 위한 jwt-decode 라이브러리 설치

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,7 +1,65 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import { jwtDecode } from 'jwt-decode'
 
+import { refresh } from './refresh'
+
+import { clearToken, getRefreshToken, setToken } from '../utils/tokenUtils'
+
+interface AxiosCustomRequestConfig extends AxiosRequestConfig {
+  retryCount: number
+}
+
+const MAX_RETRY_COUNT = 3
 const client = axios.create({
   baseURL: 'https://ssudate-server.yourssu.com/',
 })
+
+client.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error) => {
+    const originRequest = error.config as AxiosCustomRequestConfig
+    originRequest.retryCount = originRequest.retryCount ?? 0
+
+    const refreshToken = getRefreshToken()
+
+    if (error.response?.status === 401 && originRequest.url === '/refresh') {
+      return Promise.reject(error)
+    }
+
+    if (error.response?.status === 401 && originRequest.retryCount < MAX_RETRY_COUNT) {
+      try {
+        if (verifyRefreshToken(refreshToken)) {
+          const response = await refresh(refreshToken!)
+          const newAccessToken = response.accessToken
+          const newRefreshToken = response.refreshToken
+
+          setToken(newAccessToken, newRefreshToken)
+          originRequest.retryCount += 1
+
+          return client(originRequest)
+        } else {
+          clearToken()
+          window.location.assign('/')
+        }
+      } catch {
+        clearToken()
+        window.location.assign('/')
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+
+const verifyRefreshToken = (token: string | null) => {
+  if (!token) return false
+
+  const currentTime = new Date().getTime()
+  const expireTime = jwtDecode(token).exp
+
+  if (expireTime && expireTime * 1000 < currentTime) return false
+  return true
+}
 
 export default client

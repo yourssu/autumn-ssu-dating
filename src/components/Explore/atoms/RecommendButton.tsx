a15c8f06357648f6895a5bded3941358f3c88b56
intersoom <sallybig@naver.com>
2023-12-20 04:10:30 +0900

    ✨ 추천인 코드 API 연결

--- src/components/Explore/atoms/RecommendButton.tsx
+++ src/components/Explore/atoms/RecommendButton.tsx
@@ -1,15 +1,66 @@
-import { RecommendOpenType } from '../../../types/explore.type'
+import { useState } from 'react'
+
+import { AxiosError } from 'axios'
+import { useSetRecoilState } from 'recoil'
+
+import { postReferralCode } from '../../../apis/postReferralCode'
+import useRecoilToast from '../../../hooks/useRecoilToast'
+import { exploreToastAtom } from '../../../state/exploreToastAtom'
+import { ticketAtom } from '../../../state/ticketAtom'
+import { OpenType } from '../../../types/explore.type'
 import BoxButton from '../../common/BoxButton'
 import Spacing from '../../common/Spacing'
 
 interface RecommendButtonProps extends React.HTMLAttributes<HTMLDivElement> {
-  recommendOpen: RecommendOpenType
+  recommendOpen: OpenType
 }
 
 const RecommendButton = ({ recommendOpen, ...props }: RecommendButtonProps) => {
+  const setTicketCount = useSetRecoilState(ticketAtom)
+  const { setRecoilStateToast } = useRecoilToast(exploreToastAtom)
+  const [code, setCode] = useState<string>('')
+
+  async function verifyCode() {
+    try {
+      const response = await postReferralCode(code)
+      setTicketCount(response.data.ticket)
+      setRecoilStateToast({
+        isShow: true,
+        toastMessage: '추천인 코드 인증 완료! 이용권 한 장이 충전됐어요.',
+      })
+    } catch (error) {
+      const authError = error as AxiosError
+      switch (authError.response?.status) {
+        case 400:
+          setRecoilStateToast({
+            isShow: true,
+            toastMessage: '10자리의 추천인 코드를 입력해주세요.',
+          })
+          break
+
+        case 404:
+          setRecoilStateToast({
+            isShow: true,
+            toastMessage: '존재하지 않는 추천인 코드예요.',
+          })
+          break
+
+        default:
+          setRecoilStateToast({
+            isShow: true,
+            toastMessage: '추천인 코드를 다시 한번 확인해주세요.',
+          })
+          break
+      }
+    }
+  }
   return (
     <span {...props}>
-      <BoxButton isLine={recommendOpen === 'opened' ? 'line' : 'filled'} size="middle">
+      <BoxButton
+        isLine={recommendOpen === 'opened' ? 'line' : 'filled'}
+        size="middle"
+        className="flex-col"
+      >
         {recommendOpen === 'opened' ? (
           <div className="animate-fade self-start animate-normal animate-duration-300 animate-once animate-ease-linear">
             <Spacing direction="vertical" size={12} />
@@ -17,9 +68,13 @@ const RecommendButton = ({ recommendOpen, ...props }: RecommendButtonProps) => {
             <Spacing direction="vertical" size={8} />
             <div className="flex w-full items-end">
               <input
+                value={code}
                 className="h-[27px] w-[152px] border-b-[1px] py-[4px] text-body2 text-pink focus:outline-none"
                 onClick={(e) => {
                   e.stopPropagation()
+                }}
+                onChange={(e) => {
+                  setCode(e.target.value)
                 }}
               ></input>
               <Spacing direction="horizontal" size={8} />
@@ -27,6 +82,7 @@ const RecommendButton = ({ recommendOpen, ...props }: RecommendButtonProps) => {
                 className="flex h-[23px] w-[42px] items-center justify-center rounded bg-pink"
                 onClick={(e) => {
                   e.stopPropagation()
+                  verifyCode()
                 }}
               >
                 <span className="text-caption text-white">확인</span>

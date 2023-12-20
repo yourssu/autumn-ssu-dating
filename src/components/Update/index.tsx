ad3cd03eaf7fb5f9d351bab3af4d79822a289249
nijuy <youjin6325@naver.com>
2023-12-21 03:32:30 +0900

    💄 수정 페이지 spacing 위치 변경

--- src/components/Update/index.tsx
+++ src/components/Update/index.tsx
@@ -91,92 +91,94 @@ const UpdateProfile = () => {
   return (
     <>
       {!isLoading && (
-        <div className="flex h-[calc(100%-44px-34px)] select-none flex-col items-center justify-between overflow-y-scroll scrollbar-hide">
-          <div className="grid gap-y-6 text-body2">
-            <Spacing direction="vertical" size={24} />
-            <div>
-              <label className="flex w-full items-center justify-between">
-                닉네임
-                <span className="text-caption text-gray">
-                  ( <span className="text-pink">{formData.nickName?.length}</span> / 9 )
-                </span>
-              </label>
-              <Spacing direction="vertical" size={8} />
-              <InputField
-                required
-                width={342}
-                maxLength={9}
-                placeholder="ex. 숭실대 뿌슝이"
-                value={formData.nickName}
-                onChange={(e) => updateFields({ nickName: (e.target as HTMLInputElement).value })}
-              />
-            </div>
+        <>
+          <Spacing direction="vertical" size={24} />
+          <div className="flex h-[calc(100%-44px-34px)] select-none flex-col items-center justify-between overflow-y-scroll scrollbar-hide">
+            <div className="grid gap-y-6 text-body2">
+              <div>
+                <label className="flex w-full items-center justify-between">
+                  닉네임
+                  <span className="text-caption text-gray">
+                    ( <span className="text-pink">{formData.nickName?.length}</span> / 9 )
+                  </span>
+                </label>
+                <Spacing direction="vertical" size={8} />
+                <InputField
+                  required
+                  width={342}
+                  maxLength={9}
+                  placeholder="ex. 숭실대 뿌슝이"
+                  value={formData.nickName}
+                  onChange={(e) => updateFields({ nickName: (e.target as HTMLInputElement).value })}
+                />
+              </div>
 
-            <div>
-              <label>
-                MBTI <span className="text-caption text-gray">(클릭해서 MBTI를 완성해보세요!)</span>
-              </label>
-              <Spacing direction="vertical" size={8} />
-              <div className="flex justify-between">
-                {!_.isEmpty(mbtiValueObject) &&
-                  mbtiOptions.map((option, index) => (
-                    <RadioSelector
-                      key={index}
-                      labels={option}
-                      updateMbti={(value: string) => {
-                        setMbtiValueObject((prev) => {
-                          return { ...prev, ...{ [index]: value } }
-                        })
-                      }}
-                      defaultLabel={mbtiValueObject[index.toString()]}
-                    />
-                  ))}
+              <div>
+                <label>
+                  MBTI
+                  <span className="text-caption text-gray">(클릭해서 MBTI를 완성해보세요!)</span>
+                </label>
+                <Spacing direction="vertical" size={8} />
+                <div className="flex justify-between">
+                  {!_.isEmpty(mbtiValueObject) &&
+                    mbtiOptions.map((option, index) => (
+                      <RadioSelector
+                        key={index}
+                        labels={option}
+                        updateMbti={(value: string) => {
+                          setMbtiValueObject((prev) => {
+                            return { ...prev, ...{ [index]: value } }
+                          })
+                        }}
+                        defaultLabel={mbtiValueObject[index.toString()]}
+                      />
+                    ))}
+                </div>
+              </div>
+
+              <div className="grid">
+                <label className="flex w-full items-center justify-between">
+                  본인 매력 어필
+                  <span className="text-caption text-gray">
+                    ( <span className="text-pink">{formData.introduce?.length}</span> / 100 )
+                  </span>
+                </label>
+                <Spacing direction="vertical" size={8} />
+                <TextareaField
+                  required={true}
+                  maxLength={100}
+                  placeholder="ex. 프로필을 가져갈 다른 사람들에게 내 매력을 어필해보세요! (모두가 사용하는 커뮤니티인만큼 부적절한 언어 사용은 피해주세요 :D)"
+                  value={formData.introduce}
+                  onChange={(e) => updateFields({ introduce: e.target.value })}
+                />
+              </div>
+
+              <div>
+                <label>연락처 (인스타그램 / 카카오톡 ID / 전화번호 중 택 1)</label>
+                <Spacing direction="vertical" size={8} />
+                <InputField
+                  required
+                  width={342}
+                  placeholder="ex. @yourssu_official"
+                  value={formData.contact}
+                  onChange={(e) => updateFields({ contact: (e.target as HTMLInputElement).value })}
+                />
               </div>
             </div>
+            <BoxButton isDisabled={canUpdate ? 'abled' : 'disabled'} isLine="filled" size="large">
+              <button
+                type="submit"
+                className="h-full w-full disabled:cursor-not-allowed"
+                disabled={!canUpdate}
+                onClick={onSubmit}
+              >
+                수정하기
+              </button>
+            </BoxButton>
 
-            <div className="grid">
-              <label className="flex w-full items-center justify-between">
-                본인 매력 어필
-                <span className="text-caption text-gray">
-                  ( <span className="text-pink">{formData.introduce?.length}</span> / 100 )
-                </span>
-              </label>
-              <Spacing direction="vertical" size={8} />
-              <TextareaField
-                required={true}
-                maxLength={100}
-                placeholder="ex. 프로필을 가져갈 다른 사람들에게 내 매력을 어필해보세요! (모두가 사용하는 커뮤니티인만큼 부적절한 언어 사용은 피해주세요 :D)"
-                value={formData.introduce}
-                onChange={(e) => updateFields({ introduce: e.target.value })}
-              />
-            </div>
-
-            <div>
-              <label>연락처 (인스타그램 / 카카오톡 ID / 전화번호 중 택 1)</label>
-              <Spacing direction="vertical" size={8} />
-              <InputField
-                required
-                width={342}
-                placeholder="ex. @yourssu_official"
-                value={formData.contact}
-                onChange={(e) => updateFields({ contact: (e.target as HTMLInputElement).value })}
-              />
-            </div>
+            {stateToast && <ToastMessage>{stateToast}</ToastMessage>}
           </div>
-          <Spacing direction="vertical" size={8} />
-          <BoxButton isDisabled={canUpdate ? 'abled' : 'disabled'} isLine="filled" size="large">
-            <button
-              type="submit"
-              className="h-full w-full disabled:cursor-not-allowed"
-              disabled={!canUpdate}
-              onClick={onSubmit}
-            >
-              수정하기
-            </button>
-          </BoxButton>
-
-          {stateToast && <ToastMessage>{stateToast}</ToastMessage>}
-        </div>
+        </>
       )}
     </>
   )
